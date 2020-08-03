acd0abc1cd376663640173e9d60b51b46562e860
radude89 <radu.ionut.dan@gmail.com>
2020-08-03 11:09:52 +0300

    Completed day 46

---
+++ ArrowChallenge/ArrowChallenge/ColorCyclingRect.swift
@@ -0,0 +1,38 @@
+//
+//  ColorCyclingRect.swift
+//  ArrowChallenge
+//
+//  Created by Radu Dan on 03/08/2020.
+//  Copyright © 2020 Radu Dan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ColorCyclingRect: View {
+    var amount = 0.0
+    var steps = 100
+
+    var body: some View {
+        ZStack {
+            ForEach(0..<steps) { value in
+                SwiftUI.Rectangle()
+                    .inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom), lineWidth: 2)
+            }
+            .drawingGroup()
+        }
+    }
+
+    func color(for value: Int, brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+
+        return Color(hue: targetHue, saturation: 1, brightness: brightness)
+    }
+}
---
+++ ArrowChallenge/ArrowChallenge/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  ArrowChallenge
+//
+//  Created by Radu Dan on 03/08/2020.
+//  Copyright © 2020 Radu Dan. All rights reserved.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Rectangle: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+                
+        path.move(to: CGPoint(x: rect.minX + insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX + insetAmount, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX + insetAmount, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct ContentView: View {
+    @State private var colorCycle = 0.0
+    @State private var insetAmount: CGFloat = 5
+    
+    var body: some View {
+        VStack {
+            ColorCyclingRect(amount: self.colorCycle)
+                .frame(width: 300, height: 300)
+            
+            Slider(value: $colorCycle)
+                .padding()
+            
+            VStack(spacing: 0) {
+                Triangle()
+                    .fill(Color.red)
+                    .frame(width: 20, height: 20)
+                
+                Rectangle(insetAmount: insetAmount)
+                    .fill(Color.red)
+                    .frame(width: 20, height: 100)
+                    .onTapGesture {
+                        withAnimation {
+                            self.insetAmount = CGFloat.random(in: 1...9)
+                        }
+                    }
+            }
+        }
+    .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
